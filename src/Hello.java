daa1ff95be9353292c872b35c08c475a1183457b
DENIZ <deniz.kaymal@gmail.com>
2020-11-16 19:45:19 -0500

    added LoginUtility

--- src/Hello.java
+++ src/Hello.java
@@ -2,10 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-        //line 1
+        // line 1
         //line 2
         //line 3
-
 
 
 
